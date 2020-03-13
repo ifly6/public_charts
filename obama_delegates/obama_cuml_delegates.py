f324f2f928ae7003ac85d6df0d77893a8ee51a46
ifly6 <ifly6@me.com>
2020-03-13 16:12:59 -0400

    update delegates, add charts here

--- obama_delegates/obama_cuml_delegates.py
+++ obama_delegates/obama_cuml_delegates.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -22,7 +23,7 @@ def parse_date(s):
             d = pd.to_datetime(s, format='%b %d')
         except ValueError:
             d = pd.to_datetime(s, format='%B %d')
-        
+
     d = d.replace(year=2008)
     return d
 
@@ -31,23 +32,26 @@ dels['date'] = dels['date'].apply(parse_date)
 for c in ['barack_obama', 'hillary_rodham_clinton']:
     dels[c] = pd.to_numeric(dels[c], errors='coerce').fillna(0)
 
-cumulative_dels = pd.concat([dels['date'], dels['barack_obama'].cumsum(), 
+cumulative_dels = pd.concat([dels['date'], dels['barack_obama'].cumsum(),
                              dels['hillary_rodham_clinton'].cumsum()], axis=1)
 cumulative_dels = cumulative_dels.groupby('date').max().reset_index()
 cumulative_dels['obama_down_by'] = cumulative_dels.eval('hillary_rodham_clinton - barack_obama')
 
-
 # pictures
 f, a = plt.subplots(1, 1, figsize=(11, 8.5))
-a.plot(cumulative_dels['date'], cumulative_dels['barack_obama'])
-a.plot(cumulative_dels['date'], cumulative_dels['hillary_rodham_clinton'])
+a.plot(cumulative_dels['date'], cumulative_dels['barack_obama'], label='Barack Obama')
+a.plot(cumulative_dels['date'], cumulative_dels['hillary_rodham_clinton'], label='Hillary Clinton')
 a.set_title('Cumulative Democratic delegates in 2008')
 a.legend()
 
 f1, a1 = plt.subplots(1, 1, figsize=(11, 8.5))
 a1.plot(cumulative_dels['date'], cumulative_dels['obama_down_by'])
-a1.set_title('Barack Obama, down by how many delegates? (Negative means ahead)')
+a1.set_title('Barack Obama, down by how many delegates?\n(Negative means Obama ahead)')
+a1.plot(pd.date_range(cumulative_dels['date'].min().value, cumulative_dels['date'].max().value,
+                      periods=50),
+        np.repeat(0, 50),
+        linestyle='--', color='gray')
 
-f.savefig('delegates.png')
-f1.savefig('obama.png')
+f.savefig('delegates.png', bbox_inches='tight')
+f1.savefig('obama.png', bbox_inches='tight')
 cumulative_dels.to_csv('cumlt_dels.csv', index=False)
